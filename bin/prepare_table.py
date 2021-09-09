e19e153193df63245ca08783f66481140804d1c0
lmtani <lucas.biotec07@gmail.com>
2021-09-09 19:45:44 -0300

    feat: JSON with summary data

---
+++ bin/prepare_table.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+
+def read_one_line(path):
+    with open(path) as f:
+        content = f.read().strip()
+    return content
+
+def parse_two_lines_metrics(path, keys: int, values: int):
+    h = open(path)
+    lines = h.readlines()
+    header = lines[keys].strip().split()
+    values = lines[values].strip().split()
+    h.close()
+    return {k:v for k,v in zip(header, values)}
+
+
+def main(dup_metrics, wgs_metrics, contamination_metrics):
+    dup = parse_two_lines_metrics(dup_metrics, 6, 7)
+    wgs = parse_two_lines_metrics(wgs_metrics, 6, 7)
+    contam = parse_two_lines_metrics(contamination_metrics, 0, 1)
+
+    with open("summary.json", "w") as f:
+        json.dump(dict(**dup, **wgs, **contam), f, indent=4) 
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
